d1e16cfcfd789b6060e63de047e4a9c1cb9c29b6
Michel Casabianca <casa@sweetohm.net>
2018-02-11 16:18:20 +0100

    Added template for Go Webservice

---
+++ golangws/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func setupRouter() *gin.Engine {
+	router := gin.Default()
+	router.GET("/hello/:name", func(ctx *gin.Context) {
+		name := ctx.Param("name")
+		ctx.String(http.StatusOK, "Hello %s!", name)
+	})
+	return router
+}
+
+func main() {
+	router := setupRouter()
+	router.Run(":8000")
+}

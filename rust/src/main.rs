e7aec66faf27a3f54b8e19a3cd5e209c55255921
Michel Casabianca <mcasabianca@sfbx.io>
2024-03-04 18:39:47 +0100

    Added Rust template

---
+++ rust/src/main.rs
@@ -0,0 +1,24 @@
+use clap::Parser;
+
+/// Describe project here
+#[derive(Parser)]
+#[command(version)]
+struct Cli {
+    /// Describe argument here
+    #[clap(default_value = "test")]
+    arg: String,
+}
+
+fn main() {
+    // parse command line arguments
+    let args = Cli::parse();
+    // print argument
+    println!("Argument: {}", args.arg);
+    // run function
+    run();
+}
+
+/// Describe function here
+fn run() {
+    panic!("Crash and burn!")
+}
